8a2103e773b76c4fd6476772e0cb6ec2e2169a9d
amin roosta <amin.roosta@outlook.com>
2020-12-02 16:12:15 +0800

    run yarn

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,41 +5,27 @@ const yosay = require('yosay');
 
 module.exports = class extends Generator {
   prompting() {
-    // Have Yeoman greet the user.
     this.log(
       yosay(`Welcome to the finest ${chalk.red('generator-parcel-ts')} generator!`)
     );
-
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
   }
 
   writing() {
-	// Copy all non-dotfiles
-	  this.fs.copy(
-		  this.templatePath('**/*'),
-		  this.destinationRoot()
-	  );
+	this.fs.copy(
+	  this.templatePath('**/*'),
+	  this.destinationRoot()
+	);
 
-	  // Copy all dotfiles
-	  this.fs.copy(
-		  this.templatePath('.*'),
-		  this.destinationRoot()
-	  );
+	this.fs.copy(
+	  this.templatePath('.*'),
+	  this.destinationRoot()
+	);
   }
 
   install() {
-    this.installDependencies();
+    this.installDependencies({
+      yarn: {force: true},
+      npm: false
+    });
   }
 };
